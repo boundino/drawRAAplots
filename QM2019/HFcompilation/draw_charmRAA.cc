2c0934af7f9b31b8be455239f923f66200377513
Jing <boundino@gmail.com>
2019-11-01 15:57:19 +0800

    up

--- QM2019/HFcompilation/draw_charmRAA.cc
+++ QM2019/HFcompilation/draw_charmRAA.cc
@@ -7,29 +7,33 @@
 #include "xjjrootuti.h"
 #include "xjjcuti.h"
 #include "getdat.h"
+#include "Ch_RAA.h"
 
 std::map<std::string, exps::getdat*> dats = {
-  std::pair<std::string, exps::getdat*>("STAR_D0_RAA_pt_0-10" , new exps::getdat("dataset/dat_charmRAA/STAR_D0_RAA_pt_0-10.dat" , xjjroot::mycolor_satmiddle["orange"], "#bf{#it{STAR} AuAu} 200 GeV" , "0-10% D^{0}" , 21, 0.5, "log")),
-  std::pair<std::string, exps::getdat*>("ALICE_D_RAA_pt_0-10" , new exps::getdat("dataset/dat_charmRAA/ALICE_D_RAA_pt_0-10.dat" , xjjroot::mycolor_satmiddle["red"], "#bf{#it{ALICE} AuAu} 5 TeV" , "0-10% Average D" , 20, 0.5, "log")),
+  std::pair<std::string, exps::getdat*>("STAR_D0_RAA_pt_0-10" , new exps::getdat("dataset/dat_charmRAA/STAR_D0_RAA_pt_0-10.dat" , xjjroot::mycolor_satmiddle["green"], "#bf{#it{STAR} AuAu} 200 GeV" , "0-10% D^{0}" , 21, 0.05, "log")),
+  std::pair<std::string, exps::getdat*>("ALICE_D_RAA_pt_0-10" , new exps::getdat("dataset/dat_charmRAA/ALICE_D_RAA_pt_0-10.dat" , xjjroot::mycolor_satmiddle["red"], "#bf{#it{ALICE} PbPb} 5 TeV" , "0-10% Average D" , 20, 0.05, "log")),
+  std::pair<std::string, exps::getdat*>("CMS_D0_RAA_pt_0-10" , new exps::getdat("dataset/dat_charmRAA/CMS_D0_RAA_pt_0-10.dat" , xjjroot::mycolor_satmiddle["yellow"], "#bf{#it{CMS} PbPb} 5 TeV" , "0-10% D0" , 20, 0.05, "log")),
 };
 
 void draw_charmRAA_pt(const std::vector<std::string>& idraw, std::string outname, float ymax)
 {
   std::map<std::string, bool> ifdraw;
-  // std::vector<std::string> datorder = {"ALICE_D_RAA_pt_0-10", "CMS_D_RAA_pt_0-10", "STAR_D0_RAA_pt_0-10", "STAR_ce_RAA_pt_0-10"};
-  std::vector<std::string> datorder = {"ALICE_D_RAA_pt_0-10", "STAR_D0_RAA_pt_0-10"};
+  // std::vector<std::string> legorder = {"ALICE_D_RAA_pt_0-10", "CMS_D_RAA_pt_0-10", "STAR_D0_RAA_pt_0-10", "STAR_ce_RAA_pt_0-10"};
+  std::vector<std::string> legorder = {"ALICE_D_RAA_pt_0-10", "CMS_D0_RAA_pt_0-10", "STAR_D0_RAA_pt_0-10"};
+  exps::Ch_RAA* chRAA = new exps::Ch_RAA("dataset/dat_chRAA/Ch_RAA_pt_0-10.dat");
+
   for(auto& dd : dats) 
     { 
-      dd.second->setxw(0.3); 
+      // dd.second->setxw(0.5); 
       ifdraw[dd.first] = false;
     }
   for(auto& tt : idraw) { ifdraw[tt] = true; }
 
-  TH2F* hempty = new TH2F("hempty", ";p_{T} (GeV/c);R_{AA}", 10, 0.1, 200, 10, 0, ymax);
+  TH2F* hempty = new TH2F("hempty", ";p_{T} (GeV/c);R_{AA}", 10, 0.3, 200, 10, 0, ymax);
   xjjroot::sethempty(hempty, 0, 0);
-  TLegend* leg = new TLegend(0.40, 0.85-0.04*8, 0.60, 0.85);
+  TLegend* leg = new TLegend(0.60, 0.85-0.04*8, 0.80, 0.85);
   xjjroot::setleg(leg, 0.03);
-  for(auto& dd : datorder)
+  for(auto& dd : legorder)
     {
       if(ifdraw[dd]) leg->AddEntry(dats[dd]->grsyst(), dats[dd]->line1(), "fp");
       else leg->AddEntry((TObject*)0, "", NULL);
@@ -37,13 +41,18 @@ void draw_charmRAA_pt(const std::vector<std::string>& idraw, std::string outname
       else leg->AddEntry((TObject*)0, "", NULL);
     }
 
-  xjjroot::setgstyle(1);
+  xjjroot::setgstyle(3);
   TCanvas* c = new TCanvas("c", "", 600, 600);
   c->SetLogx();
   hempty->Draw();
+  chRAA->Draw();
   for(auto& i : idraw)
-    { dats[i]->draw(); }
+    { dats[i]->draw("f"); }
+  for(auto& i : idraw)
+    { dats[i]->draw("p"); }
   leg->Draw();
+  xjjroot::drawline(0.3, 1, 10, 1, kBlack, 2, 2);
+  gPad->RedrawAxis();
   std::string outputname = "plots/charmRAA/ccharmRAA_pt_"+outname+".pdf";
   xjjroot::mkdir(outputname.c_str());
   c->SaveAs(outputname.c_str());
@@ -55,7 +64,7 @@ void draw_charmRAA_pt(const std::vector<std::string>& idraw, std::string outname
 
 int main()
 {
-  draw_charmRAA_pt(std::vector<std::string>({"STAR_D0_RAA_pt_0-10", "ALICE_D_RAA_pt_0-10"}), "0", 1.25);
+  draw_charmRAA_pt(std::vector<std::string>({"CMS_D0_RAA_pt_0-10", "STAR_D0_RAA_pt_0-10", "ALICE_D_RAA_pt_0-10"}), "0", 1.25);
   return 0;
 }
 
--- QM2019/HFcompilation/getdat.h
+++ QM2019/HFcompilation/getdat.h
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
 
 namespace exps
 {
@@ -10,20 +10,21 @@ namespace exps
   {
   public:
     getdat(std::string filename, int color, std::string line1, std::string line2, Style_t mstyle, float xw=0.5, const char* opt="linear"); // linear, log
-    void draw() { fgrsyst->Draw("2 same"); fgrstat->Draw("pe same"); }
-    void setxw(float xw) { fxw = xw; for(int i=0;i<fn;i++) { fgrsyst->SetPointError(i, fxw, fysyst[i]); } }
-    TGraphErrors* grsyst() { return fgrsyst; }
-    TGraphErrors* grstat() { return fgrstat; }
+    void draw(std::string opt="pf") { if(xjjc::str_contains(opt, "f")) { fgrsyst->Draw("2 same"); }
+      if(xjjc::str_contains(opt, "p")) { fgrstat->Draw("pe same"); } }
+    void setxw(float xw);
+    TGraphAsymmErrors* grsyst() { return fgrsyst; }
+    TGraphAsymmErrors* grstat() { return fgrstat; }
     const char* line1() { return fline1.data(); }
     const char* line2() { return fline2.data(); }
   private:
-    TGraphErrors *fgrstat, *fgrsyst;
+    TGraphAsymmErrors *fgrstat, *fgrsyst;
     int fn, fcolor;
     float fxw;
     Style_t fmstyle;
     std::string fline1, fline2;
     std::string fopt;
-  std::vector<float> fx, fxstat, fxsyst, fy, fystat, fysyst;
+    std::vector<float> fx, fxstat, fxsyst, fy, fystat, fysystl, fysysth;
   };
 }
 
@@ -37,28 +38,40 @@ exps::getdat::getdat(std::string filename, int color, std::string line1, std::st
 {
   std::ifstream getdata(filename.c_str());
   fn = 0;
-  bool ll = fopt=="log";
+  bool linear = (fopt=="linear");
   while(true)
     {
-      float xx, yy, stat, syst, temp;
+      float xx, yy, stat, systl, systh, temp;
       getdata >> xx;
       if(getdata.eof()) break;
       getdata >> yy
               >> temp >> stat
-              >> temp >> syst;
+              >> temp >> systl
+      >> temp >> systh;
       fx.push_back(xx);
       fxstat.push_back(0);
-      fxsyst.push_back(ll?fxw:fxw*xx);
+      fxsyst.push_back(linear?fxw:(fxw*xx));
       fy.push_back(yy);
       fystat.push_back(fabs(stat-yy));
-      fysyst.push_back(fabs(syst-yy));
+      fysystl.push_back(fabs(systl-yy));
+      fysysth.push_back(fabs(systh-yy));
       fn++;
     }
-  fgrstat = new TGraphErrors(fn, fx.data(), fy.data(), fxstat.data(), fystat.data());
+  fgrstat = new TGraphAsymmErrors(fn, fx.data(), fy.data(), fxstat.data(), fxstat.data(), fystat.data(), fystat.data());
   fgrstat->SetName(Form("grstat_%s", filename.c_str()));
   xjjroot::setthgrstyle(fgrstat, fcolor, fmstyle, 1.2, fcolor, 1, 2);
-  fgrsyst = new TGraphErrors(fn, fx.data(), fy.data(), fxsyst.data(), fysyst.data());
+  fgrsyst = new TGraphAsymmErrors(fn, fx.data(), fy.data(), fxsyst.data(), fxsyst.data(), fysystl.data(), fysysth.data());
   fgrsyst->SetName(Form("grsyst_%s", filename.c_str()));
   // xjjroot::setthgrstyle(fgrsyst, fcolor, fmstyle, 1.2, fcolor, 1, 2, fcolor, 0.3, 1001, 1);
   xjjroot::setthgrstyle(fgrsyst, fcolor, fmstyle, 1.2, fcolor, 1, 2, fcolor, 0.3, 1001, 0.1);
+}
+
+void exps::getdat::setxw(float xw)
+{
+  fxw = xw;
+  for(int i=0;i<fn;i++)
+    {
+      double thisxw = fopt=="linear"?fxw:(fxw*fx[i]);
+      fgrsyst->SetPointError(i, thisxw, thisxw, fysystl[i], fysysth[i]);
+    }
 }
