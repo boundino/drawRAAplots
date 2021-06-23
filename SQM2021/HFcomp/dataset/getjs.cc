edc26af1677fb64bc9a5e0aee41c9a174adb416d
Jing Wang <boundino@gmail.com>
2021-06-23 17:29:56 -0400

    up getjs

--- SQM2021/HFcomp/dataset/getjs.cc
+++ SQM2021/HFcomp/dataset/getjs.cc
@@ -5,32 +5,52 @@
 #include "hfdata.h"
 #include "xjjcuti.h"
 
+#include "cvt.h"
+
 class item
 {
 public:
-  item(std::vector<std::string> a);
+  item(std::map<std::string, std::string>& a);
   std::string operator[](std::string key) { return fa[key]; }
 private:
   std::map<std::string, std::string> fa;
   std::vector<std::string> fva = {"inputfile", "ref",
                                   "obs", "xtitle", "particle", 
                                   "system", "energy", "collab", 
-                                  "kinea", "kineb", "kinec", 
+                                  "kinea", "kineb", 
                                   "link"};
   void print();
   int width(std::string a)
   {
     if(a=="ref") return 23;
-    else if(a=="tag") return 30;
-    else if(a=="xtitle") return 14;
-    else if(a=="inputfile") return 40;
-    else if(a=="particle") return 14;
-    else return 10;
+    else if(a=="tag") return 70;
+    else if(a=="particle") return 20;
+    else if(xjjc::str_contains(a, "kine")) return 20;
+    else return 8;
   }
 };
 
-int getjs(item it)
+int getjs(std::string input)
 {
+  std::vector<std::string> a = xjjc::str_divide(input, "[]");
+  for(auto ia : a) ia = xjjc::str_erasetwospace(ia);
+  std::map<std::string, std::string> b;
+  //
+  b["inputfile"] = a[0]; // inputfile
+  b["ref"] = a[1]; // ref
+  b["link"] = a[2];
+  std::string name = xjjc::str_erasestar(a[0], "*/");
+  name = xjjc::str_erasestar(name, ".*");
+  std::vector<std::string> vn = xjjc::str_divide(name, "_");
+  b["particle"] = cvt::parseparticle(vn[0]); // particle
+  b["obs"] = vn[1]; // obs
+  b["xtitle"] = vn[2]; // xtitle
+  b["system"] = vn[3]; // system
+  b["energy"] = cvt::parseenergy(vn[4]); // energy
+  b["collab"] = vn[5]; // collab
+  b["kinea"] = cvt::parsekine(vn[6]);
+  b["kineb"] = cvt::parsekine(vn[7]);
+  item it(b);
   exps::hfdata* dat = new exps::hfdata(it["inputfile"], kBlack, "", "", 20);
 
   std::ofstream fout(Form("js/%s.js", it["tag"].c_str()));
@@ -45,7 +65,6 @@ int getjs(item it)
   fout << "    collab : \"" << it["collab"] << "\"," << std::endl;
   fout << "    kinea : \"" << it["kinea"] << "\"," << std::endl;
   fout << "    kineb : \"" << it["kineb"] << "\"," << std::endl;
-  fout << "    kinec : \"" << it["kinec"] << "\"," << std::endl;
   fout << "    data : [" << std::endl;
 
   for(int i=0; i<dat->n(); i++)
@@ -69,18 +88,16 @@ int getjs(item it)
 
 int main(int argc, char* argv[])
 {
-  std::vector<std::string> a = xjjc::str_divide(argv[1], "[]");
-  item arg(a);
-  return getjs(arg);
+  return getjs(argv[1]);
 }
 
-item::item(std::vector<std::string> a)
+item::item(std::map<std::string, std::string>& a)
 {
   for(int i=0; i<fva.size(); i++)
-    fa[fva[i]] = a[i]; 
+    fa[fva[i]] = a[fva[i]]; 
 
   fva.insert(fva.begin(), "tag");
-  fa["tag"] = xjjc::str_erasestar(a[0], "*/");
+  fa["tag"] = xjjc::str_erasestar(a["inputfile"], "*/");
   fa["tag"] = xjjc::str_erasestar(fa["tag"], ".*");
 
   print();
